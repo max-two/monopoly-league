b7d48031ce64f2080b9fb0c013dff9cd035de655
Max McKinley <max.mck@hey.com>
2025-04-30 03:33:31 -0400

    Add game constraints

--- backend/app/models/game.py
+++ backend/app/models/game.py
@@ -1,11 +1,11 @@
 from sqlmodel import Field, SQLModel
 
 class GameBase(SQLModel):
-    week: int
+    week: int = Field(ge=1, le=22)
     home_team: str
     away_team: str
-    home_score: int | None = None
-    away_score: int | None = None
+    home_score: int | None = Field(default=None, ge=0)
+    away_score: int | None = Field(default=None, ge=0)
 
 class Game(GameBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -18,6 +18,6 @@ class CreateGame(GameBase):
 
 
 # TODO: 
-# Add constraints (week #'s)
+# Make teams enums
 # Make types strict i.e. can't add extra fields to a create
 # UUIDs
